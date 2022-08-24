729b12e055426311b3b700d09588767e5e338272
Vlad Lazoryk <50370915+VladLazoryk@users.noreply.github.com>
2022-08-24 15:30:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=2.99.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = var.subscriptionID
+  tenant_id       = var.tenantID
+  features {}
+}
